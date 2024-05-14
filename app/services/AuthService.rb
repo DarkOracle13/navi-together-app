0675e4f39010b1e4ed6be4233c513210a388aa01
JReyG <jansen121202@gmail.com>
2024-05-14 20:10:38 +0800

    feat : front end to back end, account page, flashbar
    feat : front end to back end, account page, flashbar

--- app/services/AuthService.rb
+++ app/services/AuthService.rb
@@ -1,8 +1,22 @@
+require 'http'
+
 module Cryal
-    module AuthService
-        def self.authenticate(params)
-            puts "Authenticating with #{params.params['username']} and #{params.params['password']}"
-            # use logger to log the params
+    class AuthService
+        class UnauthorizedError < StandardError; end
+
+        def initialize(config)
+            @config = config
+        end
+
+        def authenticate(routing)
+            response = HTTP.post("http://localhost:3000/api/v1/auth/authentication",
+                            json: { username: routing.params['username'], password: routing.params['password']})
+
+            raise(UnauthorizedError) unless response.code == 200
+
+            body = JSON.parse(response.body)
+            data = JSON.parse(body['data']) if body['data']
+            data
         end
     end
 end
